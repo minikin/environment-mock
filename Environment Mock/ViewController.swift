8da745d05f3aefdfbc74a9ef57afc265bc365504
Sasha Prokhorenko <djminikin@gmail.com>
2019-04-03 09:28:08 +0200

    Initial Commit

---
+++ Environment Mock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Environment Mock
+//
+//  Created by Sasha Prokhorenko on 03.04.19.
+//  Copyright © 2019 Sasha Prokhorenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Environment MockTests/Environment_MockTests.swift
@@ -0,0 +1,34 @@
+//
+//  Environment_MockTests.swift
+//  Environment MockTests
+//
+//  Created by Sasha Prokhorenko on 03.04.19.
+//  Copyright © 2019 Sasha Prokhorenko. All rights reserved.
+//
+
+import XCTest
+@testable import Environment_Mock
+
+class Environment_MockTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
